5d8c919e230e1d738d72ab07325b563d1aad3a47
shanlihou <shanlihou@gmail.com>
2015-01-21 20:20:49 +0800

    first

---
+++ Widgets.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2014 LiuLang <gsushzhsosgsu@gmail.com>
+# Use of this source code is governed by GPLv3 license that can be found
+# in http://www.gnu.org/licenses/gpl-3.0.html
+
+
+from gi.repository import Gtk
+
+class LeftLabel(Gtk.Label):
+    '''左对齐的标签'''
+
+    def __init__(self, label):
+        print('/usr/local/lib/python3.4/dist-packages/bcloud/Widgets.py:__init__ 10')
+        super().__init__(label)
+        self.props.xalign = 0.0
+
+
+class SelectableLeftLabel(LeftLabel):
+    '''左对齐的标签, 标签内容可选中'''
+
+    def __init__(self, label):
+        print('/usr/local/lib/python3.4/dist-packages/bcloud/Widgets.py:__init__ 18')
+        super().__init__(label)
+        self.props.selectable = True
+
+
+class IconButton(Gtk.Button):
+    '''在gtk3.14中, 引入了gtk_button_new_from_icon_name() 这个方法'''
+
+    def __init__(self, icon_name, size=Gtk.IconSize.BUTTON):
+        print('/usr/local/lib/python3.4/dist-packages/bcloud/Widgets.py:__init__ 26')
+        super().__init__()
+        img = Gtk.Image.new_from_icon_name(icon_name, size)
+        self.set_image(img)
